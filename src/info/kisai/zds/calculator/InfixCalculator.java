ddcc1c45e86f1112d6ddc68678874789e1955bb3
SpaceFox <ujueseo.yeou@gmail.com>
2015-10-13 00:05:22 +0200

    tmp

--- src/info/kisai/zds/calculator/InfixCalculator.java
+++ src/info/kisai/zds/calculator/InfixCalculator.java
@@ -1,5 +1,7 @@
 package info.kisai.zds.calculator;
 
+import info.kisai.zds.calculator.Token.Type;
+
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,12 +12,17 @@ import java.util.Queue;
  */
 public class InfixCalculator {
 
-    public double eval(String in) {
+    private RPNCalculator rpnCalculator = new RPNCalculator();
+
+    public double eval(String in) throws ParenthesisMismatchException, UnknownElementException, NotEnoughOperandsException {
         String rpnNotation = shuntingYard(in);
-        return 0;
+
+        System.out.println(rpnNotation);
+
+        return rpnCalculator.eval(rpnNotation);
     }
 
-    private String shuntingYard(String in) {
+    private String shuntingYard(String in) throws ParenthesisMismatchException {
 
         List<Token> tokens = tokenize(in);
 
@@ -25,10 +32,59 @@ public class InfixCalculator {
         final Deque<Token> stack = new LinkedList<>();
 
         for (Token token : tokens) {
-            token.process(queue, stack);
+            switch (token.getType()) {
+                case NUMBER:
+                    queue.add(token);
+                    break;
+                case FUNCTION:
+                    stack.push(token);
+                    break;
+                case FUNCTION_ARG_SEPARATOR:
+                    while (stack.getFirst().getType() != Type.OPEN_PARENTHESIS) {
+                        queue.add(stack.pop());
+                    }
+                    break;
+                case OPERATOR:
+                    final Operator o1 = RPNCalculator.OPERATORS.get(token.getValue());
+                    Operator o2 = stack.isEmpty() ? null : RPNCalculator.OPERATORS.get(stack.getFirst().getValue());
+                    while ( o2 != null
+                        &&  (   (o1.isLeftAssociative()   && o1.getPrecedence() <= o2.getPrecedence())
+                            ||  (!o1.isLeftAssociative()  && o1.getPrecedence() < o2.getPrecedence())
+                        )
+                    ) {
+                        queue.add(stack.pop());
+                        o2 = stack.isEmpty() ? null : RPNCalculator.OPERATORS.get(stack.getFirst().getValue());
+                    }
+                    stack.add(token);
+                    break;
+                case OPEN_PARENTHESIS:
+                    stack.push(token);
+                    break;
+                case CLOSE_PARENTHESIS:
+                    while (stack.getFirst().getType() != Type.OPEN_PARENTHESIS) {
+                        queue.add(stack.pop());
+                    }
+                    stack.pop();
+                    if (stack.getFirst().getType() == Type.FUNCTION) {
+                        queue.add(stack.pop());
+                    }
+                    break;
+            }
+            System.out.println(queue + "\t\t" + stack);
+        }
+        while (!stack.isEmpty()) {
+            if (stack.getFirst().getType() == Type.OPEN_PARENTHESIS) {
+                throw new ParenthesisMismatchException(in);
+            }
+            queue.add(stack.pop());
         }
 
-        return null;
+        StringBuilder buffer = new StringBuilder();
+        for (Token token : queue) {
+            buffer.append(token.getValue()).append(" ");
+        }
+
+        return buffer.toString();
     }
 
     private List<Token> tokenize(String in) {
@@ -37,38 +93,38 @@ public class InfixCalculator {
         List<Token> out = new LinkedList<>();
         char[] chars = in.toCharArray();
 
-        TokenType previousTokenType = null, type;
+        Type previousType = null, type;
         boolean isSpaceChar;
         for (char c : chars) {
 
             type = null;
             isSpaceChar = Character.isSpaceChar(c);
-            if (TokenType.TYPE_DISCRIMINANTS.containsKey(c)) {
-                type = TokenType.TYPE_DISCRIMINANTS.get(c);
+            if (Token.TYPE_DISCRIMINANTS.containsKey(c)) {
+                type = Token.TYPE_DISCRIMINANTS.get(c);
             } else if (!isSpaceChar) {
-                type = TokenType.FUNCTION;
+                type = Type.FUNCTION;
             }
-            if (previousTokenType == TokenType.NEGATIVE_OR_OPERATOR) {
+            if (previousType == Type.NEGATIVE_OR_OPERATOR) {
                 final Token lastToken = out.get(out.size() - 1);
                 // Supposition : on a pas 2 nombres à la suite. Permet d'interpréter les formes comme "1-1".
-                if (type != TokenType.NUMBER || (lastToken != null && lastToken.getType() == TokenType.NUMBER)) {
-                    out.add(new Token(TokenType.OPERATOR, "-"));
+                if (type != Type.NUMBER || (lastToken != null && lastToken.getType() == Type.NUMBER)) {
+                    out.add(new Token(Type.OPERATOR, "-"));
                     buffer = new StringBuilder();
                 }
             }
-            if (    previousTokenType != null
-                &&  previousTokenType != TokenType.NEGATIVE_OR_OPERATOR
-                && (isSpaceChar || type != previousTokenType)
+            if (    previousType != null
+                &&  previousType != Type.NEGATIVE_OR_OPERATOR
+                && (isSpaceChar || type != previousType)
             ) {
-                out.add(new Token(previousTokenType, buffer.toString()));
+                out.add(new Token(previousType, buffer.toString()));
                 buffer = new StringBuilder();
             }
-            previousTokenType = type;
+            previousType = type;
             if (!isSpaceChar) {
                 buffer.append(c);
             }
         }
-        out.add(new Token(previousTokenType, buffer.toString()));
+        out.add(new Token(previousType, buffer.toString()));
         return out;
     }
 }
--- src/info/kisai/zds/calculator/Token.java
+++ src/info/kisai/zds/calculator/Token.java
@@ -1,26 +1,55 @@
 package info.kisai.zds.calculator;
 
-import java.util.Deque;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by spacefox on 06/10/15.
  */
 public class Token {
 
-    private TokenType type;
+    public enum Type {
+        NUMBER,
+        FUNCTION,
+        FUNCTION_ARG_SEPARATOR,
+        OPERATOR,
+        OPEN_PARENTHESIS,
+        CLOSE_PARENTHESIS,
+        NEGATIVE_OR_OPERATOR,
+        ;
+    }
+    public static final Map<Character, Type> TYPE_DISCRIMINANTS = new HashMap<>();
+    static {
+        TYPE_DISCRIMINANTS.put('0', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('1', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('2', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('3', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('4', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('5', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('6', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('7', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('8', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('9', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('.', Type.NUMBER);
+        TYPE_DISCRIMINANTS.put('-', Type.NEGATIVE_OR_OPERATOR);
+        TYPE_DISCRIMINANTS.put('+', Type.OPERATOR);
+        TYPE_DISCRIMINANTS.put('*', Type.OPERATOR);
+        TYPE_DISCRIMINANTS.put('/', Type.OPERATOR);
+        TYPE_DISCRIMINANTS.put('^', Type.OPERATOR);
+        TYPE_DISCRIMINANTS.put(',', Type.FUNCTION_ARG_SEPARATOR);
+        TYPE_DISCRIMINANTS.put('(', Type.OPEN_PARENTHESIS);
+        TYPE_DISCRIMINANTS.put(')', Type.CLOSE_PARENTHESIS);
+    }
+
+    private Type type;
     private String value;
 
-    public Token(TokenType type, String value) {
+    public Token(Type type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public void process(Queue<Token> queue, Deque<Token> stack) {
-        type.process(this, queue, stack);
-    }
-
-    public TokenType getType() {
+    public Type getType() {
         return type;
     }
 
@@ -30,9 +59,10 @@ public class Token {
 
     @Override
     public String toString() {
-        return "Token{" +
-                "type=" + type +
-                ", value='" + value + '\'' +
-                '}';
+//        return"Token{" +
+//                "type=" + type +
+//                ", value='" + value + '\'' +
+//                '}';
+        return value;
     }
 }
